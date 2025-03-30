e5b6eb6e4c9b0affcd6c7599aaec4719d7fefa16
cyanxianv <christianjbvinluan@gmail.com>
2025-03-30 14:33:58 +0800

    Add files via upload

---
+++ PHYTON LAB ACT6.py
@@ -0,0 +1,10 @@
+DATA = {
+"AHRI": "The Nine-Tailed Fox.",
+"YASUO": "The Unforgiven.",
+"JINX": "The Loose Cannon.",
+"TEEMO": "The Swift Scout.",
+"LUX":"The Lady of Luminosity",
+}
+
+a = input("CHAMPION:")
+print(DATA[a])
